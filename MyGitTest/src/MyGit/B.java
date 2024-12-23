b12c9db9d8e0598cf198f9eb561954c0d5c31258
sungyeon <tjddus0731@naver.com>
2024-12-23 18:37:40 +0900

    Initial setting

---
+++ MyGitTest/src/MyGit/B.java
@@ -0,0 +1,7 @@
+package MyGit;
+
+public class B {
+	public static void main(String args[]) {
+		System.out.println("B 생성");
+	}
+}
---
+++ MyGitTest/src/MyGit/C.java
@@ -0,0 +1,7 @@
+package MyGit;
+
+public class C {
+	public static void main(String args[]) {
+		System.out.println("C 생성");
+	}
+}
